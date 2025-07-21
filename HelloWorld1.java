0ae0bf74b0ce30c0535c0562ef7be9c0c0b67352
Md Ariful Islam Concol <mdarifulislamconcol01@gmail.com>
2025-07-20 22:49:06 -0600

    Update HelloWorld1.java

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //Print Hello World
 
 public class HelloWorld1 {
@@ -6,10 +6,3 @@ public class HelloWorld1 {
     System.out.println("\nHello World.");
   }
 }
-=======
-public class HelloWorld{
-  public static void main(String[] args) {
-  System.out.println("HelloWorld.");
-  }
-}
->>>>>>> 5b1bc818097d51ee45e12e3e7864fb367cd86e52
